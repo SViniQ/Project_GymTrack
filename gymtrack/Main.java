f5e025b0430820463f354f102a077d7c78cab04a
Dereck Portela <dereck_alexander@outlook.com>
2025-04-07 21:25:51 -0300

    adiciona metodos a classe aluno e instancia um objeto aluno em main

--- gymtrack/Main.java
+++ gymtrack/Main.java
@@ -3,5 +3,9 @@ package gymtrack;
 public class Main {
     public static void main(String[] args) {
        System.out.println("Bem vindo ao Gym Track!");
+
+       Aluno aluno = new Aluno("Dereck", 31, "dereck@test.com", "99999999999", "999999999", 'M', 81.5, 1.82, 20.5);
+        System.out.println("Info dos alunos: ");
+        aluno.showInfo();
     }
 }
